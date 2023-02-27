eddbbc2c3cf7a21cf65048130bed10e76a2f4ea7
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-27 12:32:24 +0300

    Returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,5 +9,8 @@ def number_of_subscribers(subreddit):
     """ Returns the number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(sys.argv[1])
     req = requests.get(url)
+    if req.status_code != 200:
+        return 0
     js = req.json()
+
     return js.get('data').get('subscribers')
