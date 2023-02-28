d26a9ce10dd50ef0300ac9694b0a7a2d0220112b
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-28 10:49:04 +0300

    Some updates

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,43 +2,22 @@
 """ Get top 10 hot posts """
 import requests
 
-"""
-def get_posts(response):
-
-    posts = []
-
-    for post in response.json().get('data').get('children'):
-        posts.append(post.get('data').get('title'))
-
-    return posts """
-
 
 def top_ten(subreddit):
     """ Returns top 10 posts """
-    client = 'LqbShrCIO2ZLZQRZt7s6Fg'
-    secret = 'RpCwbohdiD_6h2KLPVU_y-_TJCSMZQ'
-    url = 'https://www.reddit.com/api/v1/access_token'
-    auth = requests.auth.HTTPBasicAuth(client, secret)
-    data = {'grant_type': 'password',
-            'username': 'yitbehisbro',
-            'password': 'Yitbe1234%'}
-
     headers = {'User-Agent': 'PPP/0.0.1'}
-    res = requests.post(url, auth=auth, data=data, headers=headers)
-    TOKEN = res.json()['access_token']
-    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
     params = {'limit': 10}
-    url = "https://oauth.reddit.com/r/{}/top/?t=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     posts = []
 
     for i in range(1):
-        res = requests.get(url, headers=headers, params=params)
+        res = requests.get(url, headers=headers, params=params,
+                           allow_redirects=False)
         if res.status_code != 200 or res.status_code == 301:
             print("None")
             return
         for post in res.json().get('data').get('children'):
             posts.append(post.get('data').get('title'))
-        # posts = get_posts(res)
 
     for title in posts:
         print(title)
