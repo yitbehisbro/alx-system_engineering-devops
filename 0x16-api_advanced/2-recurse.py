a244300c411ea4a544dfaf7557934501baf654ef
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-28 10:56:13 +0300

    Some updates

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,22 +5,10 @@ import requests
 
 def recurse(subreddit, hot_list=[]):
     """ Returns top posts """
-    client = 'LqbShrCIO2ZLZQRZt7s6Fg'
-    secret = 'RpCwbohdiD_6h2KLPVU_y-_TJCSMZQ'
-    url = 'https://www.reddit.com/api/v1/access_token'
-    auth = requests.auth.HTTPBasicAuth(client, secret)
-    data = {'grant_type': 'password',
-            'username': 'yitbehisbro',
-            'password': 'Yitbe1234%'}
-
     headers = {'User-Agent': 'PPP/0.0.1'}
-    res = requests.post(url, auth=auth, data=data, headers=headers)
-    TOKEN = res.json()['access_token']
-    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
-
-    url = "https://oauth.reddit.com/r/{}/top".format(subreddit)
-    res = requests.get(url, headers=headers)
-    if res.status_code != 200:
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200 or res.status_code == 301:
         return None
 
     js = res.json()
