f4b32b63c51c1d7975d15b5b7c25536337b57f15
yitbehisbro <yitbarekw@hu.edu.et>
2023-03-01 10:57:00 +0300

    Some updates

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,13 +8,13 @@ def recurse(subreddit, hot_list=[]):
     headers = {'User-Agent': 'PPP/0.0.1'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
-        return "OK"
+    if res.status_code != 200 or res.status_code == 301:
+        return None
 
     js = res.json()
     posts = js.get('data').get('children')
     if not posts:
-        return "OK"
+        return None
     hot_list.append(posts[len(hot_list)].get('data').get('title'))
     if len(hot_list) == 0:
         return "OK"
